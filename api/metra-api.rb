4b9d0a5dc975e2ced6c7211795633fddb749a78e
Robert Guico <rguico@gmail.com>
2015-03-22 17:01:17 -0500

    Adding webpack.

--- api/metra-api.rb
+++ api/metra-api.rb
@@ -3,20 +3,26 @@ require 'rubygems'
 
 Bundler.require
 
-require 'sinatra'
+require 'sinatra/base'
 require 'yaml'
 require 'sinatra/json'
 require 'sinatra/config_file'
 
-before do
-    content_type :json
-end
+module MetraLiveSchedule
+    class App < Sinatra::Base
+        before do
+            content_type :json
+        end
 
-get '/lines' do
-    lines = YAML.load_file('lines.yaml')
-    return lines
-end
+        get '/lines' do
+            lines = YAML.load_file('lines.yaml')
+            return lines
+        end
 
-after do
-    response.body = json response.body
+        after do
+            response.body = json response.body
+        end
+
+        run! if app_file == $0
+    end
 end
