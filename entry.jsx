6dd84db19357f38f6d8e2b693343592d52ca991a
Robert Guico <rguico@gmail.com>
2015-03-23 19:48:39 -0500

    Experiments in webpack.

---
+++ entry.jsx
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react');
+var LineHeader = require('./js/elements/line-header.jsx');
+
+React.renderComponent(<LineHeader />, document.getElementById('content'));
